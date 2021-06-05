8de9a250e45b4c8d84f0c0501471bd381cb9f38a
injun.hwang <injun.hwang@deliveryhero.co.kr>
2021-06-05 22:05:40 +0900

    Add flag

--- redis_client.py
+++ redis_client.py
@@ -8,7 +8,9 @@ startup_nodes = [
     {"host": "test-cluster-0002-001.lkqmf4.0001.apn2.cache.amazonaws.com", "port": "6379"},
     {"host": "test-cluster-0003-001.lkqmf4.0001.apn2.cache.amazonaws.com", "port": "6379"},
 ]
-redis_cluster_client = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+redis_cluster_client = RedisCluster(
+    startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True
+)
 #
 # def redis_instance(read_only: bool = False) -> StrictRedis:
 #     return _get_redis_instance(read_only)
